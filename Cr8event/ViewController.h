60092b4402503453a62f7bc16e5e6c536cd28335
csliphone <mehulprabtani@Mehul-Prabtanis-Mac-mini.local>
2014-05-30 15:51:23 +0530

    Added Project

---
+++ Cr8event/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cr8event
+//
+//  Created by Hrushikesh betai on 12/30/13.
+//  Copyright (c) 2013 Compubits Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Cr8event/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Cr8event
+//
+//  Created by Hrushikesh betai on 12/30/13.
+//  Copyright (c) 2013 Compubits Solutions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
